da71c54eef67d3298ee244f7027018809b7f48ce
izhan9637 <izhan9637@gmail.com>
2020-04-06 23:44:26 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => {
+  return (
+    
+    <div>
+      <Link to='/tests'>TestsPage</Link>
+      <h1>HATS PAGE </h1>
+    </div>
+  );
+};
+
+const TestsPage = () => {
+  return (
+    <div>
+      <h1>Tests PAGE </h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/tests' component={TestsPage} />
+
+      </Switch>
     </div>
   );
 }
