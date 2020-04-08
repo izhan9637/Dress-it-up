dcdbb980408b07e7d4a0f4a60bf2f04d85537986
izhan9637 <izhan9637@gmail.com>
2020-04-08 23:05:56 +0530

    Added collection-preview and collection-item component

--- src/App.js
+++ src/App.js
@@ -4,33 +4,15 @@ import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => {
-  return (
-    
-    <div>
-      <Link to='/tests'>TestsPage</Link>
-      <h1>HATS PAGE </h1>
-    </div>
-  );
-};
-
-const TestsPage = () => {
-  return (
-    <div>
-      <h1>Tests PAGE </h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
-        <Route exact path='/tests' component={TestsPage} />
-
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
